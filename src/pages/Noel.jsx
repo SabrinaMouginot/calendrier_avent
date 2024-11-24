9d1ed88a30953c60a42442389de75e9d90e11b3e
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-11-24 15:10:17 +0100

    creation de la page Père Noel

---
+++ src/pages/Noel.jsx
@@ -0,0 +1,13 @@
+import "./Home.css"; // Réutilise le CSS de Home
+
+const Noel = () => {
+    return (
+        <main>
+            {/* Ajout des couches de neige */}
+            <div className="snow"></div>
+            <div className="snowbis"></div>
+        </main>
+    );
+};
+
+export default Noel;
