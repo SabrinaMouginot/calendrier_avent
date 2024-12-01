ff40caed6fc27f3da91b98e4a5a4f7769a26c294
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-12-01 03:48:03 +0100

    ajout d un son sur la page Noel

--- src/pages/Noel/Noel.jsx
+++ src/pages/Noel/Noel.jsx
@@ -1,8 +1,10 @@
-import "../pages/Home/Home.css";
+import "../Home/Home";
 
 const Noel = () => {
+
     return (
         <main>
+            <audio src="/jingle-bells-bells.mp3" autoPlay loop />
             <div className="snow"></div>
             <div className="snowbis"></div>
         </main>
