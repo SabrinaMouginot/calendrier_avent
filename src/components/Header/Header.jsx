8aa42b515a7626227ca0970e82a6f6cae4936630
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-12-01 04:20:11 +0100

    travail de style sur le header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,50 +1,4 @@
-import "../Header/Header.css";
-
-const Header = () => {
-  return (
-    <header className="header">
-      <h1>Calendrier de l’Avent</h1>
-      <nav>
-        <ul className="nav-links">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/Noel">Père Noël</a>
-          </li>
-
-          <li className="dropdown">
-            <a href="/About">À propos</a>
-            <ul className="dropdown-menu">
-              <li>
-                <a href="/About">Le projet</a>
-                {/* <a href="#project">Le projet</a> */}
-              </li>
-              <li>
-                <a href="#features">Fonctionnalités</a>
-              </li>
-              <li>
-                <a href="#technologies">Technologies</a>
-              </li>
-              <li>
-                <a href="#about-me">À propos de moi</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </li>
-
-        </ul>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;
-
-
-// import "./Header.css";
+// import "../Header/Header.css";
 
 // const Header = () => {
 //   return (
@@ -55,11 +9,16 @@ export default Header;
 //           <li>
 //             <a href="/">Home</a>
 //           </li>
+//           <li>
+//             <a href="/Noel">Père Noël</a>
+//           </li>
+
 //           <li className="dropdown">
-//             <a href="/about">À propos</a>
+//             <a href="/About">À propos</a>
 //             <ul className="dropdown-menu">
 //               <li>
-//                 <a href="#project">Le projet</a>
+//                 <a href="/About">Le projet</a>
+//                 {/* <a href="#project">Le projet</a> */}
 //               </li>
 //               <li>
 //                 <a href="#features">Fonctionnalités</a>
@@ -75,6 +34,7 @@ export default Header;
 //               </li>
 //             </ul>
 //           </li>
+
 //         </ul>
 //       </nav>
 //     </header>
@@ -82,3 +42,66 @@ export default Header;
 // };
 
 // export default Header;
+
+import { NavLink } from "react-router-dom";
+import "../Header/Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Calendrier de l’Avent</h1>
+      <nav>
+        <ul className="nav-links">
+          {/* Onglet Home */}
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          {/* Onglet Père Noël */}
+          <li>
+            <NavLink
+              to="/Noel"
+              className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
+            >
+              Père Noël
+            </NavLink>
+          </li>
+
+          {/* Onglet À propos avec menu déroulant */}
+          <li className="dropdown">
+            <NavLink
+              to="/About"
+              className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
+            >
+              À propos
+            </NavLink>
+            <ul className="dropdown-menu">
+              <li>
+                <a href="/About">Le projet</a>
+              </li>
+              <li>
+                <a href="#features">Fonctionnalités</a>
+              </li>
+              <li>
+                <a href="#technologies">Technologies</a>
+              </li>
+              <li>
+                <a href="#about-me">À propos de moi</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
